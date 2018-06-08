13e5354cb2904b7673b162780c5f1ec4ab0b8721
unknown <sai.3898@gmail.com>
2018-06-08 16:02:02 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -12,29 +12,60 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/users',(req,res)=>{
-// console.log(req.body);
-var users = new Users({
-    email : req.body.email,
-    password:req.body.password
-});
+  // console.log(req.body);
+  var users = new Users({
+      email : req.body.email,
+      password:req.body.password
+  });
+  
+  users.save().then((doc)=>{
+  res.send(doc);
+  }, (e) => {
+   res.status(400).send(e);
+  });
+  });
+  
+  app.get('/users',(req,res)=>{
+    Users.find().then((users)=>{
+     res.send({users});
+    }, (e)=>{
+      res.status(400).send(e);
+    })
+  });
+  
+  
+  app.listen(3000,()=>{
+      console.log('Started on port 3000');
+  });
+  
+  
 
-users.save().then((doc)=>{
-res.send(doc);
-}, (e) => {
- res.status(400).send(e);
-});
-});
-
-app.get('/users',(req,res)=>{
-  Users.find().then((users)=>{
-   res.send({users});
-  }, (e)=>{
-    res.status(400).send(e);
-  })
-});
 
 
-app.listen(3000,()=>{
-    console.log('Started on port 3000');
-});
+
+//todo (or)
+
+// var todo = new Todo({
+//     text : req.body.text
+// });
+
+// todo.save().then((doc)=>{
+// res.send(doc);
+// }, (e) => {
+//  res.status(400).send(e);
+// });
+// });
+
+// app.get('/todos',(req,res)=>{
+//   Todo.find().then((todo)=>{
+//    res.send({todos});
+//   }, (e)=>{
+//     res.status(400).send(e);
+//   })
+// });
+
+
+// app.listen(3000,()=>{
+//     console.log('Started on port 3000');
+// });
 
