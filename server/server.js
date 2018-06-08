fad242d96657fd7d69dc3152148d92973b03935b
unknown <sai.3898@gmail.com>
2018-06-08 18:42:53 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} =require('mongodb');
 
 
 var {mongoose} = require('./db/mongoose');
@@ -33,6 +33,31 @@ app.post('/users',(req,res)=>{
     })
   });
   
+  //GET /users/123
+  app.get('/users/:id', (req,res)=>{
+    // res.send(req.params);
+    var id =req.params.id;
+
+if (!ObjectID.isValid(id)) {
+  return res.status(404).send();
+}
+    // Valid id using isValid
+    //404-send  back empty send
+
+    Users.findById(id).then((users)=>{
+     if (!users){
+       return res.status(404).send();
+     }
+
+     res.send({users});
+    }).catch((e)=>{
+      res.status(400).send();
+    })
+    //fingById
+    //success
+    //error
+      //400 - 
+  });
   
   app.listen(3000,()=>{
       console.log('Started on port 3000');
